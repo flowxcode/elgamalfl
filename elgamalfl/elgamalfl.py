58b8c4ca9c5bf716bd1f18fc26201c369990b9d9
flowxcode <florian.johannes.isopp@gmail.com>
2020-12-05 11:45:50 +0100

    sage approach of prime

--- elgamalfl/elgamalfl.py
+++ elgamalfl/elgamalfl.py
@@ -2,8 +2,26 @@
 # enc
 # dec
 
+import sys
+
+from sage.all import *
+
 def generate_p():
-    return 13
+    smoothest = 2^30 # just something large
+    for p in primes(2^24, 2^25):
+        smoothness = 0
+        for f, e in (p - 1).factor():
+            if e != 1:
+                smoothness = 2^30
+                break
+            smoothness += f
+        if smoothness < smoothest:
+            smooth = p
+            smoothest = smoothness
+        print(smooth, smooth - 1, (smooth - 1).factor())
+
+    p = sagemath.random_prime(2^8-1, False, 2^7)
+    return p
 
 
 def generate_g():
@@ -32,3 +50,21 @@ class ElKey:
     def add_trick(self, trick):
         self.tricks.append(trick)
 
+
+def main():
+   
+
+    key = ElKey("testname")
+
+    # generate properties for elgamal
+    print(key)
+    print(vars(key))
+
+
+
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
